b5b9a0f8ef7a051028dc03b185bbf64a83788e3e
amir-surereach <amir.hanis@sure-reach.com>
2026-01-06 11:59:50 +0800

    Ziggy deploy issue

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,34 +4,35 @@ import '../css/app.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import { ZiggyVue } from 'ziggy-js';
+import { Ziggy } from './ziggy';
 
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 
-//swal alart
 import VueSweetalert2 from 'vue-sweetalert2';
-// If you don't need the styles, do not connect
 import 'sweetalert2/dist/sweetalert2.min.css';
 
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue')
+        ),
     setup({ el, App, props, plugin }) {
-        const app =  createApp({ render: () => h(App, props) })
-            app.use(plugin)
-            app.use(ZiggyVue, Ziggy)
-            app.use(ElementPlus)
-            app.use(VueSweetalert2),
-            window.Swal =  app.config.globalProperties.$swal
+        const app = createApp({ render: () => h(App, props) });
 
-            app.mount(el)
+        app.use(plugin);
+        app.use(ZiggyVue, Ziggy);
+        app.use(ElementPlus);
+        app.use(VueSweetalert2);
 
-    },
-    progress: {
-        color: '#4B5563',
+        window.Swal = app.config.globalProperties.$swal;
+
+        app.mount(el);
     },
 });
